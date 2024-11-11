81ad85d5308a75c2e623484b724bb37b1dcf145d
leoRochaCarmo <leorochaengenharia@gmail.com>
2024-11-11 17:13:44 -0300

    Últimos conceitos sobre generators adicionados

--- 02.python_intermediario/22.iterable_iterator_generator.py
+++ 02.python_intermediario/22.iterable_iterator_generator.py
@@ -75,4 +75,44 @@ gen = generator_2(n=5, maximum=8)
 for n in gen:
     print(n)
 
+#%% 
+# Yield From
 
+def gen1():
+    print('COMEÇOU G1')
+    yield 1
+    yield 2
+    yield 3
+    print('ACABOU G1')
+
+def gen3():
+    print('COMEÇOU G3')
+    yield 10
+    yield 20
+    yield 30
+    print('ACABOU G3')
+
+def gen2(gen=None):
+    print('COMEÇOU G2')
+    if gen is not None:
+        yield from gen()
+    yield 4
+    yield 5
+    yield 6
+    print('ACABOU G2')
+
+g1 = gen2(gen1)
+g2 = gen2(gen3)
+g3 = gen2()
+
+for numero in g1:
+    print(numero)
+print()
+
+for numero in g2:
+    print(numero)
+print()
+
+for numero in g3:
+    print(numero)
+print()
