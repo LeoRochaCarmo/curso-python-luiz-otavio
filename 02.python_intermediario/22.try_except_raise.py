197a03ad61b6978a5e31987e40614fce3dc34d70
Leonardo Rocha <leorochaengenharia@gmail.com>
2024-11-12 07:48:04 -0300

    aula completa sobre try, except, finally e raise

--- 02.python_intermediario/22.try_except_raise.py
+++ 02.python_intermediario/22.try_except_raise.py
@@ -44,3 +44,33 @@ else:
     print('NÃO DEU ERRO')
 finally: # sempre será executado
     print('FECHAR ARQUIVO')
+
+#%%
+
+# raise - lançando exceções (erros)
+# https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
+
+# print(123)
+# raise ValueError('Deu erro') # Lança um erro
+
+def nao_aceito_zero(d):
+    if d == 0:
+        raise ZeroDivisionError('Tentativa de divisão por zero')
+    return True
+
+def deve_ser_int_ou_float(n):
+    tipo_n = type(n)
+    if not isinstance(n, (float, int)):
+        raise TypeError(
+            f'"{n}" deve ser int ou float. '
+            f'"{tipo_n.__name__}" enviado'
+        )
+    return True
+    
+def divide(n,d):
+    deve_ser_int_ou_float(n)
+    deve_ser_int_ou_float(d)
+    nao_aceito_zero(d)
+    return n / d
+
+print(divide(3,'0'))
