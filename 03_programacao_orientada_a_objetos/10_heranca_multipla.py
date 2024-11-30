a3af7b47c51063a1fab258073750f5da0c97b582
Leonardo <leorochaengenharia@gmail.com>
2024-11-30 20:22:41 -0300

    aula sobre herança múltipla e Mixin

--- 03_programacao_orientada_a_objetos/10_heranca_multipla.py
+++ 03_programacao_orientada_a_objetos/10_heranca_multipla.py
@@ -27,3 +27,34 @@
 # Use o método de classe Classe.mro()
 # Ou o atributo __mro__ (Dunder - Double Underscore)
 #%%
+# FAZENDO O DIAMANTE
+
+class A:
+    ...
+
+    def quem_sou(self):
+        print('A')
+    
+class B(A):
+    ...
+
+    def quem_sou(self):
+        print('B')
+
+class C(A):
+    ...
+
+    def quem_sou(self):
+        print('C')
+
+class D(B, C):
+    ...
+
+    def quem_sou(self):
+        print('D')
+
+d = D()
+d.quem_sou()
+
+print(D.__mro__)
+D.mro()
